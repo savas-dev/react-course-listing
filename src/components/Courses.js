aaa29b55f446749b5992465698875ba65e055f2c
Savaş SARI <138906329+savas-dev@users.noreply.github.com>
2023-10-26 20:44:51 +0300

    project files

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,24 +1,73 @@
-import React from 'react'
-import Course from './Course'
+import React, { useState } from 'react'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
-function Courses({propsCourses, removeCourse}) {
-  return (
-    <div>
-        <div>
-            <h2 className='title'>My Courses</h2>
-        </div>
+function Courses({ propsCourses, removeCourse }) {
+    const [index, setIndex] = useState(0)
+    const { content, title, price } = propsCourses[index]
 
-        <div className='course-list'>
-            {
-                propsCourses.map((course) => {
-                    return (
-                        <Course key={course.id} propsCourseDetail={course} removeOneCourse={removeCourse} />
-                    )
-                })
+    const checkIndex = (index) =>{
+        if(index < 0 ){
+            return propsCourses.length -1
+        }
+        if(index > propsCourses.length -1 ){
+            return 0
+        }
+        return index
+    }
+
+    const prevCourse = () =>{
+        setIndex((index)=>{
+            let newIndex = index - 1
+            return checkIndex(newIndex)
+        })
+    }
+
+    const nextCourse = () =>{
+        setIndex((index)=>{
+            let newIndex = index + 1
+            return checkIndex(newIndex)
+        })
+    }
+
+    const getRandomContent = () =>{
+        let randContent = Math.floor(Math.random() * propsCourses.length)
+
+        setIndex((index)=>{
+            if(randContent === index){
+                randContent = index + 1
             }
+        })
+        setIndex(checkIndex(randContent))
+    }
+
+    return (
+        <div>
+            <div className='container'>
+                <h2 className='title'>My Courses</h2>
+                <button className='randomButton' onClick={getRandomContent}>Show Random Content</button>
+            </div>
+
+            <div className='course-list'>
+                <button className='arrowIcon' onClick={prevCourse}>
+                    <FaChevronLeft />
+                </button>
+                <div className='card'>
+                    <div className=''>
+                        <h2 className='card-title'>{title}</h2>
+                        <h4 className='card-price'>{price} ₺</h4>
+                    </div>
+                    <p className='card-content'>
+                        {content}
+                    </p>
+
+                    {/* <button className='cardDeleteButton' onClick={handleDelete}>Delete</button> */}
+                </div>
+                <button className='arrowIcon' onClick={nextCourse}>
+                    <FaChevronRight />
+                </button>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Courses
